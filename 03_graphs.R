8234f5f2aba11db6e8478a38e1e87e6ca95fc666
Chris Russo <crusso@wesleyan.edu>
2018-09-11 16:02:34 -0400

    Expanded readme notes on input, and labels on graphs

--- 03_graphs.R
+++ 03_graphs.R
@@ -4,5 +4,9 @@ library(ggplot2)
 ggplot(data = Salaries,
     aes(x = salary)) + 
 	geom_histogram() + 
-	facet_grid(rank ~ sex)
+	facet_grid(rank ~ sex) + 
+	label(title = "Salary by Sex and Rank") + 
+	theme_minimal()
+	
+
 	
--- READ.md
+++ READ.md
@@ -4,3 +4,4 @@
 ### Date 9/11/18
 
 This is data from the carData package written by John Fox.
+This is an example for advaned R.
